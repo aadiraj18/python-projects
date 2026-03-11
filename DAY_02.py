05f4dcc7f3e5227da1c99120f21c991d7cd27572
aadi raj <138025309+aadiraj18@users.noreply.github.com>
2026-03-11 17:02:13 +0530

    Include success message for bio file save
    Add confirmation message after saving bio to file

--- DAY_02.py
+++ DAY_02.py
@@ -1,3 +1,4 @@
+#Social media(i.e. Instagram, Facebook, ☓ etc) Bio generator.
 import textwrap
 
 name = input("Enter your name: ").strip()
@@ -36,4 +37,5 @@ if save == 'y':
     filename = f"{name.lower().replace(' ', '_')}_bio.txt"
     with open(filename, "w", encoding="utf-8") as f:
         f.write(bio)
+
     print("File saved")
